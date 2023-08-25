f93d55864f68bd9e1adbfc97f4e4e7fc706ae678
unknown <winatahadipratama@gmail.com>
2023-08-25 11:22:47 +0700

    I've decided to make the topbar static and not fixed, because i think it actually makes the website experience more dynamic, there would be no more annoying big box on top of your screen. also added movies section for the last section of this website, i will consider again if want to add footer and maybe other refactorings, for now this is the full version

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ let deciding = document.querySelector(".deciding")
 function game (user) {
   const computer = Math.random()
   let you;
-  user == "Rock" ? you = Math.random()*0.33 : user == "Paper" ? you = 0.33 + (Math.random()*0.33) : you = 0.66 + (Math.random()*0.33) ;
+  user == "Rock" ? you = Math.random()*0.32 : user == "Paper" ? you = 0.33 + (Math.random()*0.32) : you = 0.66 + (Math.random()*0.32) ;
   if (computer < 0.33) {
     const comPicked = "Rock"
    if (you < 0.33){
@@ -59,19 +59,19 @@ play.addEventListener('click', function () {
   RedDoor.animate( [
     { transform: "translateX(-100%)", }
   ],
-  {duration: 500, easing: "ease", fill: "forwards"}
+  {duration: 700, easing: "ease-in", fill: "forwards"}
   );
 
 BluDoor.animate( [
     { transform: "translateX(100%)", },
   ],
-  {duration: 500, easing: "ease", fill: "forwards"}
+  {duration: 700, easing: "ease-in", fill: "forwards"}
   );
     front.style.opacity = "0"
     setTimeout(() => {
         front.style.display = "none";
         door.style.display = "none";},
-        500)
+        700)
 })
 
 RPS.forEach( (element) => {element.addEventListener('click', (e) => {
@@ -82,8 +82,7 @@ RPS.forEach( (element) => {element.addEventListener('click', (e) => {
         door.removeAttribute("style")
 
         const {youPicked, comPicked, result} = game(e.target.classList)
-        console.log (youPicked, comPicked, result)
-
+        
       deciding = document.querySelector(".deciding");
       deciding.innerHTML = `<img src="img/Game_RPS/RPS_${youPicked}_Red.png" alt="${youPicked}">
       <h2>${result}</h2>
@@ -102,3 +101,17 @@ RPS.forEach( (element) => {element.addEventListener('click', (e) => {
           );
     })
 })
+
+  /* movie functions */
+
+  const mercs = ["Scout", "Soldier", "Pyro", "Engineer", "Heavy", "Pyro", "Medic", "Sniper", "Spy"]
+  const mercs_vids = [`<iframe class="Scout" src="https://www.youtube.com/embed/geNMz0J9TEQ?si=SA1l-pp236WlISkT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+  `<iframe class="Soldier" src="https://www.youtube.com/embed/h42d0WHRSck?si=rGQ0lEMQA4y4XkV2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+  `<iframe class="Pyro" src="https://www.youtube.com/embed/WUhOnX8qt3I?si=BSftUNrJ3IwPOZgD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+  `<iframec class="Engineer" src="https://www.youtube.com/embed/SNgNBsCI4EA?si=lwfKhKHxdp7B974h" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+  `<iframe class="Heavy" src="https://www.youtube.com/embed/jHgZh4GV9G0?si=Do-Xm70Aqk407LL2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+  `<iframe class="Demoman" src="https://www.youtube.com/embed/han3AfjH210?si=a23HMb73GdmsH1Pu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+  `<iframe class="Medic" src="https://www.youtube.com/embed/36lSzUMBJnc?si=NA0RBmQJedyH9dQh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+  `<iframe class="Sniper" src="https://www.youtube.com/embed/9NZDwZbyDus?si=8CS4m3elV1M6XDxI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+  `<iframe class="Spy" src="https://www.youtube.com/embed/OR4N5OhcY9s?si=Dtn9tC7FHzuCokR_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,]
+  
